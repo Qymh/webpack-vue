ba67812619e4212190f4aaac556bf5113ca707b8
qymh <415570450@qq.com>
2018-05-07 17:20:14 +0800

    tsDemo

--- tsDemo/ts/test5.ts
+++ tsDemo/ts/test5.ts
@@ -18,3 +18,49 @@ function bindentity<T>(arg:T):T{
 }
 
 let mybindentity:<T>(arg:T)=>T=bindentity
+
+let mycbindentity=(arg:string):string=>{
+  return arg
+}
+
+interface ParentIndentify{
+  <T>(arg:T):T
+}
+
+function dIdentity<T>(arg:T):T{
+  return arg
+}
+
+let newdidentity:ParentIndentify=dIdentity
+
+class classIdentity<T>{
+  value:T
+  add:(x:T,y:T)=>T
+}
+
+let myTest=new classIdentity<number>()
+
+myTest.value=1
+
+interface LengthLimit{
+  length:number
+}
+
+function lengthFn<T extends LengthLimit>(arg:T):T{
+  console.log(arg.length)
+  return arg
+}
+
+function create<T>(c:{new (cons:string):T},cons:string):T{
+  return new c(cons)
+}
+
+class mycreate{
+  value:string
+  constructor(value:string){
+    this.value=value
+  }
+}
+
+let create1=create(mycreate,'123')
+console.log(create1)
